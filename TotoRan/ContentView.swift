e2cf98a94e6306be0a673cc9336854f53176136f
kosou.tei <kosou.tei@JMPC-0103.local>
2019-10-09 09:51:46 +0900

    プロジェクト作成

---
+++ TotoRan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TotoRan
+//
+//  Created by kosou.tei on 2019/10/09.
+//  Copyright © 2019 kosou.tei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
