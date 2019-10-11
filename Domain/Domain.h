9bc2425637bd2b6e7ac67f91e514089c19d42e63
kosou.tei <kosou.tei@JMPC-0103.local>
2019-10-11 10:21:07 +0900

    EmbeddedFrameWorkによりDomain層とInfra層を定義

---
+++ Domain/Domain.h
@@ -0,0 +1,19 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by kosou.tei on 2019/10/11.
+//  Copyright © 2019 kosou.tei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
---
+++ Infra/Infra.h
@@ -0,0 +1,19 @@
+//
+//  Infra.h
+//  Infra
+//
+//  Created by kosou.tei on 2019/10/11.
+//  Copyright © 2019 kosou.tei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Infra.
+FOUNDATION_EXPORT double InfraVersionNumber;
+
+//! Project version string for Infra.
+FOUNDATION_EXPORT const unsigned char InfraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Infra/PublicHeader.h>
+
+
